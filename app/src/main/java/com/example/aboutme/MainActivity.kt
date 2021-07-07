15de900af3ea56713493ffe2f725be8f31f048b5
Krysty125 <84307914+Krysty125@users.noreply.github.com>
2021-07-07 21:40:07 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val btnDone : Button = findViewById(R.id.btn_Done)
+
+        btnDone.setOnClickListener() {
+
+        val textInput : TextView = findViewById(R.id.nickname_edit) //what the user input in plain text
+        val textResult : TextView = findViewById(R.id.text_Result) //what will be shown at the plain text
+
+        textResult.text = textInput.text
+        textResult.visibility = View.VISIBLE //after enter nickname, click the DONE button, the textResult will only appear
+
+        btnDone.visibility = View.GONE //then the button will gone
+        textInput.visibility = View.GONE //and plain text input will gone.
     }
 }
